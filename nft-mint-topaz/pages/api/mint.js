d5e384aa7184a5691f1a3683da16a597da7c22d5
Fredrickadler <13fredrick2001@gmail.com>
2025-04-06 17:55:07 +0330

    Update mint.js

--- nft-mint-topaz/pages/api/mint.js
+++ nft-mint-topaz/pages/api/mint.js
@@ -1,1 +1,22 @@
+export default function handler(req, res) {
+ if (req.method === 'POST') {
+   res.status(200).json({
+     "fc:frame": {
+       "version": "vNext",
+       "image": "https://i.imgur.com/5lAUjyc.webp",
+       "buttons": [
+         {
+           "label": "Confirm Mint",
+           "action": "post",
+           "target": "https://nft-mint-topaz.vercel.app/"
+         }
+       ],
+       "post_url": "https://nft-mint-topaz.vercel.app/"
+     },
+     "message": "Mint request processed!"
+   });
+ } else {
+   res.status(405).json({ message: "Method not allowed" });
+ }
+}
 
