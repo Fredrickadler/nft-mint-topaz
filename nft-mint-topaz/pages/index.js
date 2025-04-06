3d8fc520bba34153d204e00e59ccea2e8966df7b
Fredrickadler <13fredrick2001@gmail.com>
2025-04-06 17:54:47 +0330

    Update index.js

--- nft-mint-topaz/pages/index.js
+++ nft-mint-topaz/pages/index.js
@@ -1,1 +1,16 @@
+import '../public/styles.css';
+import Script from 'next/script';
+
+export default function Home() {
+ return (
+   <div id="app">
+     <div className="skeleton"></div>
+     <div id="content" style={{ display: 'none' }}>
+       <h1>Mint Your NFT</h1>
+       <button id="mintButton">Confirm Mint</button>
+     </div>
+     <Script src="/script.js" strategy="afterInteractive" />
+   </div>
+ );
+}
 
